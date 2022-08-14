337be4b46a3f5a95c0fcb0fff6c771828d5283bf
catarch <alanqa-ops@outlook.com>
2022-08-14 22:21:39 +0800

    updated

--- atmk.sh
+++ atmk.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 cp $1 posts_tmp/
 tmp_pic=$1
+tmp_name=$2
 mv posts_tmp/${tmp_pic##*/} posts_sources/$2
-sed -i "10a <div onclick=\"fopen(\'$2\')\" id=\"$2\" class=\"blogs\"><img src=\"posts_sources/$2\" width=\"90%\" height=\"90%\" class=\"blog_header_img\"/>$2</div>" index.html
-touch posts/$2
+
+sed -i "10a <div onclick=\"fopen(\'$2.html\')\" id=\"$2\" class=\"blogs\"><img src=\"posts_sources/$2\" width=\"90%\" height=\"90%\" class=\"blog_header_img\"/>$2</div>" index.html
+
+touch posts/$2.html
--- atrm.sh
+++ atrm.sh
@@ -1,1 +1,4 @@
 #!/bin/sh
+mv posts/$1.html posts_backup/
+rm posts_sources/$1
+sed -i '/$1/d' index.html
