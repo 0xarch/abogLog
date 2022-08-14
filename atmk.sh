93aceabee90af07c98335f99face2a64db44b500
catarch <alanqa-ops@outlook.com>
2022-08-14 20:27:27 +0800

    auto make

--- atmk.sh
+++ atmk.sh
@@ -2,5 +2,5 @@
 cp $1 posts_tmp/
 tmp_pic=$1
 mv posts_tmp/${tmp_pic##*/} posts_sources/$2
-sed -i "10a <div onclick=\"window.open(posts/$2)\" class=\"blogs\"><img src=\"posts_sources/$2\" width=\"90%\" height=\"90%\" class=\"blog_header_img\"/>$2</div>" index.html
+sed -i "10a <div onclick=\"window.open(\'posts/$2\')\" class=\"blogs\"><img src=\"posts_sources/$2\" width=\"90%\" height=\"90%\" class=\"blog_header_img\"/>$2</div>" index.html
 touch posts/$2
