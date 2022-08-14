750ebf80024ad0256c7cbd9d589ba2aa7994f24d
catarch <alanqa-ops@outlook.com>
2022-08-14 22:39:33 +0800

    added blog 博客建立

--- scripts/js.js
+++ scripts/js.js
@@ -1,10 +1,12 @@
 function intoView(){
 	nagi.style.width="10%";
 	view.style.display="block";
+	viewBg.style.display="block";
 }
 function returnToMain(){
 	nagi.style.width="100%";
 	view.style.display="none";
+	viewBg.style.display="block";
 }
 function fopen(blog){
 	var target="posts/"+blog+"";
