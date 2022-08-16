74422d8596f5446f2d46a41615084b025b5a6ad2
catarch <alanqa-ops@outlook.com>
2022-08-16 11:11:21 +0800

    gived up

--- atrm.sh
+++ atrm.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 mv posts/$1.html posts_backup/
 rm posts_sources/$1
-sed -ie '/$1/d' index.html
-sed -ie '/$1/d' scripts/search.js
+sed -ie "/$1/d" index.html
+sed -ie "/$1/d" scripts/search.js
 tmp_num=$(cat posts/__MAIN__)
 let previous_num=$tmp_num-1
 echo $previous_num > posts/__MAIN__
--- scripts/comment.js
+++ scripts/comment.js
@@ -5,14 +5,22 @@
 		}else{
 		var toSend=document.getElementById("commenter1").value;
 		}
-		var g=new Date();
 		var p=document.createElement("p");
-		p.innerHTML="<hr>"+toSend+"<br>"+g.toUTCString();
+		p.innerHTML="<hr>"+toSend+"<br>";
 		document.getElementById("viewComment1").prepend(p);
+	}else{
+		if(haveData==1){
+			var toSend=existedData;
+		}else{
+		var toSend=document.getElementById("commenter2").value;
+		}
+		var p=document.createElement("p");
+		p.innerHTML="<hr>"+toSend+"<br>";
+		document.getElementById("viewComment2").prepend(p);
 	}
 }
 
-var blogComments="sources/commentBase";
+var blogComments="sources/commentBase.json";
 function readDataBase(){$.ajax({
    url: blogComments,//json文件位置
    type: "GET",//请方式为get
@@ -20,10 +28,31 @@ function readDataBase(){$.ajax({
    //xhrFields:'Access-Control-Allow-Origin':'*',
    success: function(data) {//请求成功完成后要执行的方法 
        //each循环 使用$.each方法遍历返回的数据date
-      $.each(data.first, function(i, item) {
-            var str = 'id:' + item.name + '性别：' + item.sex;
-            send(1,1,str)
+      $.each(data.main,function(i, item) {
+            var str = 'ID-' + item.id + '\n' + item.text;
+            if(item.blogName=="博客建立"){
+				send(1,1,str);
+			}else if(item.blogName=="第二篇测试"){
+				send(2,1,str);
+			}
        })
    }
 })
+}
+ function postDataBase(){
+        $.ajax({
+            type: "POST",
+            url: blogComments,
+            contentType: "application/text;charset=utf-8",
+            //data:{"blogName":"博客建立","id":"ADMIN","text":"测试博客评论上传"},
+            data:"1",
+            dataType: "text",
+            success:function (message) {
+                //alert("提交成功"+JSON.stringify(message));
+                alert("ok");
+            },
+            error:function (message) {
+                //alert("提交失败"+JSON.stringify(message));
+            }
+        });
 }
