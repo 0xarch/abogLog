b0ed4e943b6a319ab0ac4253454d93947f3209ac
catarch <alanqa-ops@outlook.com>
2022-08-16 08:11:32 +0800

    fixed atmk error

--- atmk.sh
+++ atmk.sh
@@ -10,4 +10,4 @@ touch posts/$2.html
 sed -i "10a <div onclick=\"fopen(\'$2.html\')\" id=\"$2\" class=\"blogs\"><img src=\"posts_sources/$2\" width=\"90%\" height=\"90%\" class=\"blog_header_img\"/>$2</div>" index.html
 sed -i "2a \"$2\"," scripts/search.js
 sed -i "1a </body></html>" posts/$2.html
-sed -i "1a <!DOCTYPE html><head><meta http-equiv="content-type" content="text/html;charset=utf-8" /><link rel="stylesheet" type="text/css" href="../sources/style.css"></head><body>\n" posts/$2.html
+sed -i "1a <!DOCTYPE html><head><meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\" /><link rel=\"stylesheet\" type=\"text/css\" href=\"../sources/style.css\"></head><body>\n" posts/$2.html
