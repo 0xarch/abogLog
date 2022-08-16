d57fef157365661ffabe62544769e6f08a635c1e
catarch <alanqa-ops@outlook.com>
2022-08-16 08:15:03 +0800

    fixed atrmNoCorrectNumber error , uploaded blog 3

--- atrm.sh
+++ atrm.sh
@@ -4,5 +4,5 @@ rm posts_sources/$1
 sed -ie '/$1/d' index.html
 sed -ie '/$1/d' scripts/search.js
 tmp_num=$(cat posts/__MAIN__)
-let previous_num=$tmp_num+1
+let previous_num=$tmp_num-1
 echo $previous_num > posts/__MAIN__
